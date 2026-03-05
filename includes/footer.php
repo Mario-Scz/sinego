8cdc7dd397546b47d21fffc5a54c413d0508341a
Mario Sánchez <mroberto.drako@gmail.com>
2026-03-04 20:43:46 -0600

    Arreglar css footer

--- includes/footer.php
+++ includes/footer.php
@@ -7,9 +7,9 @@
     <p>Creando obras, transformando ideas.</p>
   </div>
 </footer>
-<script src="../js/common.js"></script>
-<script src="../js/register.js"></script>
-<script src="../js/cart.js"></script>
-<script src="../js/favorites.js"></script>
+<script src="/js/common.js"></script>
+<script src="/js/register.js"></script>
+<script src="/js/cart.js"></script>
+<script src="/js/favorites.js"></script>
 </body>
 </html>
