f909703be57f0e34c705fce9099517763cf95299
Mario Sánchez <mroberto.drako@gmail.com>
2026-03-06 12:39:28 -0600

    Idk

--- vistas/fac-add.php
+++ vistas/fac-add.php
@@ -18,7 +18,7 @@ if (empty($_SESSION['usuario']) || $_SESSION['rol'] !== 'admin') {
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Agregar Factura</title>
-<link rel="stylesheet" href="/css/facturar.css">
+<link rel="stylesheet" href="/css/fac add.css">
 </head>
 
 <body>
